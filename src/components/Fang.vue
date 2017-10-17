11fe904cfac3b798e858c42b6e21eeac70945318
彭玉芳 <191979737@qq.com>
2017-10-17 17:07:07 +0800

    了解局部组件和全局组件

---
+++ src/components/Fang.vue
@@ -0,0 +1,16 @@
+<template>
+  <dl id="fang">
+      <dt>姓名</dt>
+      <dt>Fang</dt>
+      <dt>年龄</dt>
+      <dt>18</dt>
+      <dt>性别</dt>
+      <dt>女</dt>
+  </dl>
+</template>
+<style>
+#fang{
+    color:red;
+}
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+/*全局组件
+Vue.component('Jack',{
+  template:'<p>I am {{name}}</p>',
+  data(){
+    return {
+      name:'Jack Ma'
+    }
+  }
+})*/
+/* eslint-disable no-new */
+
+/*
+注册全局组件
+import HelloWorld from './components/HelloWorld'
+Vue.component('HelloWorld',HelloWorld)
+*/
+new Vue({
+  el: '#app',
+  template: '<App />',
+  components: { App }
+//"App":App 如果你的key与value的名字一样可以缩写成 {App}
+})
