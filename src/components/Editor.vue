2e7c3d3e63e2a30889465a1ca6139b265982a52e
彭玉芳 <191979737@qq.com>
2017-10-18 15:47:41 +0800

    重构个人信息&工作经历编写区域

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -7,37 +7,6 @@
                         <use v-bind:xlink:href="`#icon-${icons[i]}`"></use>
                     </svg>
                 </li>
-                <!--笨写法
-                            <li v-bind:class="{active: currentTab === 0}" v-on:click="currentTab = 0">
-                            <svg class="icon">
-                                <use xlink:href="#icon-shenfengzheng4"></use>
-                            </svg>
-                        </li>
-                        <li v-bind:class="{active: currentTab === 1}" v-on:click="currentTab = 1">
-                            <svg class="icon">
-                                <use xlink:href="#icon-shubao"></use>
-                            </svg>
-                        </li>
-                        <li v-bind:class="{active: currentTab === 2}" v-on:click="currentTab = 2">
-                            <svg class="icon">
-                                <use xlink:href="#icon-shu"></use>
-                            </svg>
-                        </li>
-                        <li v-bind:class="{active: currentTab === 3}" v-on:click="currentTab = 3">
-                            <svg class="icon">
-                                <use xlink:href="#icon-xiangmu"></use>
-                            </svg>
-                        </li>
-                        <li v-bind:class="{active: currentTab === 4}" v-on:click="currentTab = 4">
-                            <svg class="icon">
-                                <use xlink:href="#icon-jiangbei"></use>
-                            </svg>
-                        </li>
-                        <li v-bind:class="{active: currentTab === 5}" v-on:click="currentTab = 5">
-                            <svg class="icon">
-                                <use xlink:href="#icon-dianhua"></use>
-                            </svg>
-                        </li>-->
             </ol>
         </nav>
         <ol class="panes">
@@ -45,34 +14,10 @@
                     Tab {{i+1}}
                     </li>-->
             <li v-bind:class="{active: currentTab === 0}">
-                <h2>个人信息</h2>
-                <el-form>
-                    <el-form-item label="名称">
-                        <el-input v-model="profile.name"></el-input>
-                    </el-form-item>
-                    <el-form-item label="城市">
-                        <el-input v-model="profile.city"></el-input>
-                    </el-form-item>
-                    <el-form-item label="出生年月">
-                        <el-input v-model="profile.birth"></el-input>
-                    </el-form-item>
-                </el-form>
+                <ProfileEditor v-bind:profile="profile"/>    
             </li>
             <li v-bind:class="{active: currentTab === 1}">
-                <h2>工作经历</h2>
-                <el-form>
-                    <div class="container" v-for="(work,index) in workHistory">
-                        <el-form-item label="公司">
-                            <el-input v-model="work.company"></el-input>
-                        </el-form-item>
-                        <el-form-item label="工作内容">
-                            <el-input v-model="work.content"></el-input>
-                        </el-form-item>
-                        <i class="el-icon-circle-close" v-on:click="removeWorkHistory(index)"></i>
-                        <hr>
-                    </div>
-                    <el-button class="button" style="margin-top:20px" type="primary" v-on:click="addWorkHistory">点击添加</el-button>
-                </el-form>
+                <WorkHistoryEditor v-bind:workHistory="workHistory"/>
             </li>
             <li v-bind:class="{active: currentTab === 2}">
                 <h2>学习经历</h2>
@@ -99,7 +44,11 @@ export default {
       }
   }
 }*/
+
+import ProfileEditor from './ProfileEditor'
+import WorkHistoryEditor from './WorkHistoryEditor'
 export default {
+    components:{ProfileEditor,WorkHistoryEditor},
     data(){
         return {
             currentTab:0,
@@ -118,12 +67,7 @@ export default {
         }
     },
     methods:{
-        addWorkHistory(){
-            this.workHistory.push({company:'',content:''})
-        },
-        removeWorkHistory(index){
-            this.workHistory.splice(index,1)
-        }
+
     },
 }
 </script>
